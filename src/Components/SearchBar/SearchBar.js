881dee5f133ceac21442d6df0ce9dadc41834cff
kudraj7 <kudraj7@wp.pl>
2021-02-01 19:28:04 +0100

    APIs, search, minus

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,10 +2,31 @@ import React from 'react';
 import './SearchBar.css'
 
 class SearchBar extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            typedSong: ''
+        }
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+    search() {
+        this.props.onSearch(this.state.typedSong)
+    }
+
+    handleTermChange(event) {
+        this.setState(  {typedSong: event.target.value} );
+    }
+
     render() {
         return(
             <div className="SearchBar">
-            <input placeholder="Enter A Song, Album, or Artist" />
+            <input 
+                placeholder="Enter A Song, Album, or Artist" 
+                onChange={this.handleTermChange}
+            />
             <button className="SearchButton">SEARCH</button>
             </div>
         )
